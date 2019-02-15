9ff3b81e8014d557deaf5a97ffbea3db9dcd3eae
Martijn Tennekes <mtennekes@gmail.com>
2019-02-15 09:31:29 +0100

    fixed calculate prior

--- R/prior_functions.R
+++ R/prior_functions.R
@@ -192,11 +192,13 @@ attach_class <- function(x, cls) {
 #' Calculate the mobile location given the calculated propagation and a prior.
 #' @param prop propagation object, the result of \code{\link{process_cellplan}}
 #' @param prior prior object, the result of \code{\link{create_uniform_prior}}, \code{\link{create_prior}}, or \code{\link{create_network_prior}}
+#' @param raster raster raster object
 #' @export
-calculate_mobloc <- function(prop, prior) {
-    priordf <- prior_to_df(prior)
+calculate_mobloc <- function(prop, prior, raster) {
+    check_raster(raster)
+    priordf <- prior_to_df(prior, raster)
     prop %>%
-        left_join(prior, by = 'rid') %>%
+        left_join(priordf, by = 'rid') %>%
         mutate(pga = pag * p) %>%
         group_by(antenna) %>%
         mutate(pga = pga / sum(pga)) %>%
